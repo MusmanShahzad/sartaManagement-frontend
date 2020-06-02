cfb8b6c8d34c5f83c8deb9ce0885c62a162ba2f3
usmanShahzad <usmanshahzad641@gmail.com>
2020-06-02 14:35:35 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   graphQL: 'https://sartamanagement-backend.herokuapp.com/graphql',
-  graphQLSocket: 'ws://sartamanagement-backend.herokuapp.com/graphql',
+  graphQLSocket: 'wss://sartamanagement-backend.herokuapp.com/graphql',
   production: true
 };
